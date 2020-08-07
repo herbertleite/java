92be5035adce10e3696c558f3c4777925035ecca
herbertleite <herbertroyal@outlook.com>
2020-08-07 02:09:01 -0300

    scr

--- Academia/dados/Data.java
+++
@@ -1,9 +0,0 @@
-package dados;
-import java.util.ArrayList;
-import java.util.Calendar;
-//import java.util.GregorianCalendar;
-
-public class Data {
-    Calendar data;
-    ArrayList<Turma> classes; //max     
-}
--- Academia/dados/Turma.java
+++
@@ -1,8 +0,0 @@
-package dados;
-import java.util.ArrayList;
-
-public class Turma{      
-    public int horario;
-    public Professor professor;            //1 professor max
-    public ArrayList <Turma> listaAlunos; //5 alunos max
-}
